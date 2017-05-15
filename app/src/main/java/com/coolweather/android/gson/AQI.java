f24c02b4861ff3474547a9cd80ca3945d8e678fa
Echi <Echi_Hz@hotmail.com>
2017-05-15 18:55:45 +0800

    加入显示天气信息的功能。

---
+++ app/src/main/java/com/coolweather/android/gson/AQI.java
@@ -0,0 +1,19 @@
+package com.coolweather.android.gson;
+
+/**
+ * Created by Echi on 2017/5/15.
+ */
+
+public class AQI {
+
+    public AQICity city;
+
+    public class AQICity{
+
+        public String aqi;
+
+        public String pm25;
+
+    }
+
+}
